235a3f5efd756952108a2c0e3960026fe7bd4273
AndreiLeashenco <37697193+andreileashenco@users.noreply.github.com>
2024-06-03 10:55:59 +0300

    Update lesson1.js

--- technograd/lesson1.js
+++ technograd/lesson1.js
@@ -10,20 +10,38 @@
 // (a === b) && (c =5)
 // console.log(c);
 
-let a = "" + 1 + 0//10
-let b ="" - 1 + 0//10
-let c = true + false//flase
-let d = 6 / "3"//
-let e = "2" * "3"
-let f = 4 + 5 + "px"
-let h = "$" + 4 + 5
-let i = "4" - 2
-let k = "4px" - 2
-let l = "  -9  " + 5
-let m = "  -9  " - 5
-let n = null + 1
-let o = undefined + 1
-let p = " \t \n" - 
+// ДЗ 1
+1. Какой результат будет у выражений ниже?
+"" + 1 + 0
+"" - 1 + 0
+true + false
+6 / "3"
+"2" * "3"
+4 + 5 + "px"
+"$" + 4 + 5
+"4" - 2
+"4px" - 2
+"  -9  " + 5
+"  -9  " - 5
+null + 1
+undefined + 1
+" \t \n" - 2
+
+//START Выполнение
+let a = "" + 1 + 0 //10
+let b ="" - 1 + 0/ /-1
+let c = true + false //1
+let d = 6 / "3" //2
+let e = "2" * "3" //6
+let f = 4 + 5 + "px" //9px
+let h = "$" + 4 + 5//$45
+let i = "4" - 2//2
+let k = "4px" - 2/NaN
+let l = "  -9  " + 5//-9 5
+let m = "  -9  " - 5//-14
+let n = null + 1 //1
+let o = undefined + 1//Nan
+let p = " \t \n" - //NaN
 console.log(a);
 console.log(b);
 console.log(c);
@@ -38,3 +56,23 @@ console.log(m);
 console.log(n);
 console.log(o);
 console.log(p);
+//END Выполнение
+
+2. Что выведет данный скрипт
+
+let name = "Ilya";
+console.log( `hello ${1}` ); // ?
+console.log( `hello ${"name"}` ); // ?
+console.log( `hello ${name}` ); // ?
+//Start выролнение
+console.log( `hello ${1}` ); // hello 1
+console.log( `hello ${"name"}` ); // hello name
+console.log( `hello ${name}` ); // hello Ilya
+//END Выполнение
+
+3. Напишите программу, которая строго сравнивает 2 значения, если они не равны в 3ю переменную записать результат их побитового или(or)
+ 
+let value1 = 5;
+let value2 = 10;
+let result = (value1 !== value2) && (value1 | value2); // побитовый or только если значения не равны
+console.log(result);
