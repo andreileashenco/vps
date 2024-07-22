46d8777aeda6935a48a7647f32af9b7c8a77e025
Andrei Leashenco <andrei.leashenco@gmail.com>
2024-07-22 07:21:54 +0000

    delete

--- technograd/rest.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get ('/', (reg,res) => {
-    res.end('<h1>Hello all</h1>')
-})
-app.listen(port, () => console.log(`http://localhost:${port}`))
